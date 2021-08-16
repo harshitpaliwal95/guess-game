9d4d4f82ba912b4a75fdcf3d84f90c928c069498
Harshit Paliwal <harshitpaliwal95@gmail.com>
2021-08-16 17:35:01 +0530

    2nd project is done

--- project-2/script.js
+++ project-2/script.js
@@ -1,13 +1,44 @@
 'use strict';
+const modal = document.querySelector('.modal');
 
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
+const overlay = document.querySelector('.overlay');
+
+// use queryselectorall becouse useing all the classes in same name
+const btnCloseModal = document.querySelectorAll('.close-modal');
+
+const btnOpenModel = document.querySelectorAll('.show-modal');
+
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// console.log(btnOpenModel);
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+  btnOpenModel[i].addEventListener('click', openModel);
 }
 
-console.log('hello');
+btnCloseModal[0].addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
 
-console.log('');
+// e in function is a parameter
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
 
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-}
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModel();
+  }
+  // if (e.key === 'Escape') {
+  // if does not contan hidden class
+
+  // if (!modal.classList.contains('hidden')) {
+  //   closeModel();
+  // }
+  // }
+});
